e25db5a72473c29f74cc7ca591739e7b195c44d0
Christian Delgado <ing.delgadomontiel@gmail.com>
2017-02-06 22:59:56 -0300

    Estructura del proyecto

---
+++ app/enviroment_main.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by christian on 06/02/17.
+ */
+import {bootstrap} from "angular2/platform/browser"
+//noinspection TypeScriptCheckImport
+import {AppComponent} from "./environment_app.component"
+
+bootstrap(AppComponent);
